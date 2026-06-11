9ab5a7c5235f819e9712de308de1d24a4529eb77
gilmanovamir <gilmanovamir19@gmail.com>
2026-06-11 23:15:55 +0500

    Фикс генерации

--- src/main/java/com/example/online_bank/util/CodeGeneratorUtil.java
+++ src/main/java/com/example/online_bank/util/CodeGeneratorUtil.java
@@ -7,7 +7,7 @@ import java.util.Random;
 
 @UtilityClass
 public class CodeGeneratorUtil {
-    private final Random random = new Random();
+    private final Random RANDOM = new Random();
     private static final int ACCOUNT_NUMBER_FORMAT = 100_000;
     private static final int OTP_FORMAT = 10_000;
 
@@ -18,22 +18,22 @@ public class CodeGeneratorUtil {
      * @example было: "000001"
      * стало: "810000001"
      */
-    public static String generateAccountNumber(CurrencyCode currencyCode) {
-        return String.format(currencyCode.getCode() + "%06d", random.nextInt(ACCOUNT_NUMBER_FORMAT));
+    public String generateAccountNumber(CurrencyCode currencyCode) {
+        return String.format(currencyCode.getCode() + "%06d", RANDOM.nextInt(ACCOUNT_NUMBER_FORMAT));
     }
 
     /**
      * Устаревший метод генерации номера счета.
      */
     @Deprecated
-    public static String generateAccountNumber() {
-        return String.format("%06d", random.nextInt(ACCOUNT_NUMBER_FORMAT));
+    public String generateAccountNumber() {
+        return String.format("%06d", RANDOM.nextInt(ACCOUNT_NUMBER_FORMAT));
     }
 
     /**
      * Генерация otp кода из 4 цифр
      */
-    public static String generateVerificationCode() {
-        return String.format("%04d", random.nextInt(OTP_FORMAT));
+    public String generateVerificationCode() {
+        return String.format("%04d", RANDOM.nextInt(OTP_FORMAT));
     }
 }
